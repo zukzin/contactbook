e83f750eadb82c4ef0dbec319aa0e6edb191883d
zukzin <zt22973@essex.ac.uk>
2022-05-02 17:39:41 +0100

    Update contact-book-file1.py

--- contact-book-file1.py
+++ contact-book-file1.py
@@ -1,28 +1,27 @@
 names = ["zuki"]
 phone_numbers = ["079-968-5547"]
-names = ["zethu"]
-phone_numbers = ["074-485-7585"]
-names = ["olly"]
-phone_numbers = ["075-485-7565"]
-num = 3
+
 
 for i in range(num):
     name = input("Name: ")
     phone_number = input("Phone Number: ")
+    
     names.append(name)
     phone_numbers.append(phone_numbers)
 
-    print("\nName\t\t\tPhone Number\n")
+ print("\nName\t\t\tPhone Number\n")
 
 for i in range(num):
-    print("{}\t\t\t\{}" .format(names[i], phone_numbers[i]))
+    print("{}\t\t\t{}".format(names[i], phone_numbers[i]))
+   
 
     search_term = input("\nEnter Search Term: ")
     print("Search Resut")
+    
     if search_term in names:
         index = names.index (search_term)
         phone_number = phone_number[index]
-        print("Name {}, Phone Number: {}" ,format(search_term, phone_number))
+        print("Name: {}, Phone Number: {}",format(search_term, phone_number))
+        
     else:
         print("Name not found")
-
